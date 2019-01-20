2fae8515173bd0f0416436fe1f368ae05564a512
Nastya <npatutina@gmail.com>
2019-01-20 04:27:22 +0300

    Доработана обработка ошибок на микросервисе заказов

--- CoffeeOrder/src/main/java/com/coffee/service/recipe/RecipeServiceImpl.java
+++ CoffeeOrder/src/main/java/com/coffee/service/recipe/RecipeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.coffee.service.recipe;
 
+import com.coffee.entity.Order;
 import com.coffee.entity.Recipe;
 import com.coffee.entity.RecipeIngredient;
 import com.coffee.helpers.Builder;
@@ -8,8 +9,10 @@ import com.coffee.model.order.recipeIngredient.OnlyIngredientInfo;
 import com.coffee.repository.RecipeIngredientRepository;
 import com.coffee.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -37,26 +40,54 @@ public class RecipeServiceImpl implements RecipeService {
     @Nullable
     @Override
     public RecipeWithIngredientsInfo findRecipeById(@Nonnull Integer id) {
-        return recipeRepository.findById(id).map(Builder::buildRecipeInfoWithIngredients).orElse(null);
+        Recipe recipe;
+        try {
+            recipe = recipeRepository.findById(id).get();
+        } catch (Exception ex) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Recipe not found", ex);
+        }
+        return Builder.buildRecipeInfoWithIngredients(recipe);
     }
 
     @Override
     @Transactional
     public void deleteById(@Nonnull Integer id) {
-        Recipe ri = recipeRepository.findById(id).get();
-        for (RecipeIngredient oiInfo : ri.getRecipeIngredients()) {
+        Recipe recipe;
+        try {
+            recipe = recipeRepository.findById(id).get();
+        } catch (Exception ex) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Recipe not found", ex);
+        }
+
+        for (RecipeIngredient oiInfo : recipe.getRecipeIngredients()) {
             recipeIngredientRepository.deleteById(oiInfo.getId());
         }
-        recipeRepository.deleteById(id);
+        recipeRepository.delete(recipe);
     }
 
     @Override
     @Transactional
     public Recipe save(RecipeWithIngredientsInfo recipeInfo) {
-        Recipe recipe = recipeRepository.save(Builder.buildRecipeByInfo(recipeInfo));
+        Recipe recipe;
+        try {
+            recipe = recipeRepository.save(Builder.buildRecipeByInfo(recipeInfo));
+        } catch (Exception ex) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_ACCEPTABLE,
+                    "Error save recipe: " + ex.getCause().getCause().getMessage(), ex);
+        }
 
         for (OnlyIngredientInfo oiInfo : recipeInfo.getRecipeIngredients()) {
+            try {
             recipe.addRecipeIngredients(recipeIngredientRepository.save(Builder.buildRecipeMiniIngredientInfo(oiInfo, recipe)));
+            } catch (Exception ex) {
+                throw new ResponseStatusException(
+                        HttpStatus.NOT_ACCEPTABLE,
+                        "Error save recipe ingredient for product " + oiInfo.getProductId() + ": " + ex.getCause().getCause().getMessage(), ex);
+            }
+
         }
         return recipe;
     }
@@ -64,6 +95,14 @@ public class RecipeServiceImpl implements RecipeService {
     @Override
     @Transactional
     public Recipe save(RecipeInfo recipeInfo) {
-        return recipeRepository.save(Builder.buildRecipeByInfo(recipeInfo));
+        Recipe recipe;
+        try {
+            recipe = recipeRepository.save(Builder.buildRecipeByInfo(recipeInfo));
+        } catch (Exception ex) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_ACCEPTABLE,
+                    "Error save recipe: " + ex.getCause().getCause().getMessage(), ex);
+        }
+        return recipe;
     }
 }
